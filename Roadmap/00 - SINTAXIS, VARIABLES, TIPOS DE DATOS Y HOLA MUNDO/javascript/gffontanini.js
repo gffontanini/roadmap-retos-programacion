35e03bd0d8bb71c5746f9e3dce8e70b0460c26b3
Giuliana Fontanini <g.fontanini@outlook.com>
2024-08-13 23:19:57 -0300

    #00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/gffontanini.js
@@ -0,0 +1,19 @@
+// Web oficial JS https://developer.mozilla.org/es/docs/Web/JavaScript
+
+/* 
+Esto es un comentario
+en dos lineas :)
+*/
+
+/**
+ * Esto tambiés es un comentario
+ * en múltiples lineas
+ * pero el metodo anterior me parece más intuitivo y fácil
+ */
+
+const nombre = "Giuliana"; //Lo represento como constante porque mi nombre nunca va a cambiar :)
+let edad = 24;
+let language = "JavaScript";
+let primitivosArray = ["Number", "String", "Boolean", "Undefined", "Null"];
+
+console.log(`¡Hola, ${language}!`);
